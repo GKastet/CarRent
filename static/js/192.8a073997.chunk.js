7ee4814a5d9c3f6569f7e7bc5c4b0c3fd9fcaf79
GKastet <127600582+GKastet@users.noreply.github.com>
2023-08-15 10:36:22 +0000

    Deploying to gh-pages from @ GKastet/CarRent@34c2d2d274d063d51adebcdc7af4052644fa11bd 🚀

--- static/js/192.8a073997.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcar_rent=self.webpackChunkcar_rent||[]).push([[192],{192:function(n,t,e){e.r(t);e(791);var r=e(50),u=e(184);t.default=function(){return(0,u.jsxs)(r._,{children:[(0,u.jsx)("div",{children:"CarsPage"}),(0,u.jsx)("button",{type:"button",children:"Button"}),(0,u.jsx)("button",{type:"button",children:"LoadMore"})]})}},50:function(n,t,e){e.d(t,{_:function(){return c}});var r,u=e(168),c=e(867).ZP.div(r||(r=(0,u.Z)(["\n  padding: 20px;\n"])))}}]);
-//# sourceMappingURL=192.8a073997.chunk.js.map
