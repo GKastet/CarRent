7607fb826d383ca0115245dc179877f6fd10665e
GKastet <127600582+GKastet@users.noreply.github.com>
2023-08-18 00:32:32 +0200

    load more

--- src/components/Buttons/BtnLoadMoreStyled.js
+++ src/components/Buttons/BtnLoadMoreStyled.js
@@ -1,7 +1,22 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const BtnLoadMoreS = styled.button`
-width:80px;
-height:24px;
-color: var(--blue-hover);
-`
+  width: 100px;
+  /* height:24px; */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 50px auto 0px;
+  padding: 0 10px;
+  text-decoration: underline;
+  color: var(--blue-hover);
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 24px;
+  border-radius: 14px;
+  border-color: transparent;
+  transition: background-color 300ms ease-in;
+  &:hover {
+    background-color: lightgray;
+  }
+`;
